f6489cc16347d06b45085cafbe6578d8c021fbb6
eugbyte <eugenetham1994@gmail.com>
2025-08-23 15:23:34 +0800

    fix: qr scan

--- src/evently.client/src/routes/bookings/hosting/$gatheringId/dashboard/-components/index.ts
+++
@@ -1,4 +0,0 @@
-export { Scanner } from "./scanner.tsx";
-export { BookingsTable } from "./bookings-table.tsx";
-export { Jumbotron } from "./jumbotron.tsx";
-export { StatsCard } from "./stats-card.tsx";
