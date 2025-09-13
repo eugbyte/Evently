0ff502c526b98eeccc946948ec579f893ff89ad9
eugbyte <eugenetham1994@gmail.com>
2025-09-13 23:59:12 +0800

    chore: blob storage

---
+++ deploy/Terraform/blob-storage.tf
@@ -0,0 +1,23 @@
+﻿# Create a storage account
+resource "azurerm_storage_account" "evently_sa" {
+  name                     = "saeventlydevsea"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  # Optional: Lifecycle management for blob storage
+  blob_properties {
+    versioning_enabled = false  # Disable versioning to save costs
+    delete_retention_policy {
+      days = 7  # Minimum retention period
+    }
+  }
+}
+
+# Create a blob container
+resource "azurerm_storage_container" "evently_container" {
+  name                  = "evently-dev-images"
+  storage_account_id    = azurerm_storage_account.evently_sa.id
+  container_access_type = "public"
+}
---
+++ deploy/Terraform/container-app.tf
@@ -0,0 +1,111 @@
+﻿# Azure Container Registry (ACR)
+resource "azurerm_container_registry" "acr" {
+  name                = "acreventlyprodsea"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+locals {
+  image = "eventlyserver"
+}
+
+# [Use multiple provisoners to run multiple commands in local exec](https://tinyurl.com/mrjw6rkf)
+resource "null_resource" "publish_docker_image" {
+  triggers = {
+    always_run = timestamp()
+  }
+
+  provisioner "local-exec" {
+    environment = {
+      "IMAGE" = local.image
+    }
+    working_dir = "../.."
+    command     = "docker compose build"
+  }
+
+  provisioner "local-exec" {
+    command = "az acr login --name ${azurerm_container_registry.acr.name}"
+  }
+
+  provisioner "local-exec" {
+    command = "docker tag ${local.image} ${azurerm_container_registry.acr.login_server}/${local.image}"
+  }
+
+  provisioner "local-exec" {
+    command = "docker image ls"
+  }
+
+  provisioner "local-exec" {
+    command = "docker push ${azurerm_container_registry.acr.login_server}/${local.image}"
+  }
+
+  depends_on = [azurerm_container_registry.acr]
+}
+
+resource "azurerm_container_app_environment" "env" {
+  name                       = "ca-env-evently-prod-sea"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.analytics.id
+
+  depends_on = [null_resource.publish_docker_image]
+}
+
+resource "azurerm_user_assigned_identity" "uami" {
+  name                = "uami-evently-prod-sea"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  depends_on = [null_resource.publish_docker_image]
+}
+
+resource "azurerm_role_assignment" "acr_pull" {
+  principal_id         = azurerm_user_assigned_identity.uami.principal_id
+  role_definition_name = "AcrPull"
+  scope                = azurerm_container_registry.acr.id
+}
+
+resource "azurerm_container_app" "app" {
+  name                         = "ca-evently-prod-sea"
+  container_app_environment_id = azurerm_container_app_environment.env.id
+  resource_group_name          = azurerm_resource_group.rg.name
+  revision_mode                = "Single"
+
+  # needed for authentication when pulling from private ACR 
+  # https://azureway.cloud/azure-container-apps-creating-using-terraform-part-1/
+  registry {
+    server   = azurerm_container_registry.acr.login_server
+    identity = azurerm_user_assigned_identity.uami.id
+  }
+
+  # needed for container app to access other Microsoft Entra protected resources
+  # https://learn.microsoft.com/en-us/azure/container-apps/managed-identity?tabs=portal%2Cdotnet
+  identity {
+    type = "UserAssigned"
+    identity_ids = [
+      azurerm_user_assigned_identity.uami.id
+    ]
+  }
+
+  template {
+    container {
+      name   = "eventlyserver"
+      image  = "${azurerm_container_registry.acr.login_server}/${local.image}"
+      cpu    = 0.5
+      memory = "1Gi"
+    }
+  }
+
+  ingress {
+    allow_insecure_connections = false
+    external_enabled           = true
+    target_port                = 8080
+    traffic_weight {
+      latest_revision = true
+      # ws connection not distributed to message queues yet, so it has to be 100%
+      percentage = 100
+    }
+  }
+}
