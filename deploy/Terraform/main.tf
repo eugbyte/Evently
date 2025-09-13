710aa9e10a6c0f3f7cc0225becfdfaf9de69a8be
eugbyte <eugenetham1994@gmail.com>
2025-09-13 15:20:24 +0800

    chore: update

--- deploy/Terraform/main.tf
+++ deploy/Terraform/main.tf
@@ -9,10 +9,20 @@
   }
 }
 
-locals {
-  environment = "staging"
-  project_name = "evently"
-}
 provider "azurerm" {
   features {}
+}
+
+# Define a resource group for your resources
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-evently-dev-sea"
+  location = "southeastasia"
+}
+
+resource "azurerm_log_analytics_workspace" "analytics" {
+  name                = "log-storeonwheels-prod-sea"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
 }
