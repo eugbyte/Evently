91da77b468491953017367b0e3142600447b9d36
eugbyte <eugenetham1994@gmail.com>
2025-09-13 17:10:00 +0800

    chore: terraform

---
+++ deploy/Terraform/sql-database.tf
@@ -0,0 +1,27 @@
+﻿resource "random_password" "admin_password" {
+  count       = var.sql_admin_password == null ? 1 : 0
+  length      = 20
+  special     = true
+  min_numeric = 1
+  min_upper   = 1
+  min_lower   = 1
+  min_special = 1
+}
+
+locals {
+  admin_password = try(random_password.admin_password[0].result, var.sql_admin_password)
+}
+
+resource "azurerm_mssql_server" "server" {
+  name                         = "sql-evently-prod-sea"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  administrator_login          = var.sql_admin_username
+  administrator_login_password = local.admin_password
+  version                      = "12.0"
+}
+
+resource "azurerm_mssql_database" "db" {
+  name      = "evently"
+  server_id = azurerm_mssql_server.server.id
+}
---
+++ deploy/Terraform/variables.tf
@@ -0,0 +1,14 @@
+﻿# set TF_VAR_sql_admin_username in .env
+variable "sql_admin_username" {
+  type        = string
+  description = "The administrator username of the SQL logical server."
+  default     = "azureadmin"
+}
+
+# set TF_VAR_sql_admin_password in .env
+variable "sql_admin_password" {
+  type        = string
+  description = "The administrator password of the SQL logical server."
+  sensitive   = true
+  default     = null
+}
